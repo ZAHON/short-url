d2392fc58fd8b1e60ff63bb6c1fd65f0cd9acef1
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-07-07 01:17:19 +0200

    fix: z-index issue in Header component on links page

--- app/links/components/header/header.tsx
+++ app/links/components/header/header.tsx
@@ -4,7 +4,7 @@ import { UserMenu } from '..';
 
 export function Header() {
   return (
-    <div className="sticky top-0 z-40 border-b border-b-accent-2 bg-white">
+    <div className="sticky top-0 z-30 border-b border-b-accent-2 bg-white">
       <header className="flex h-14 items-center justify-between px-4 lg:mx-auto lg:max-w-7xl">
         <Link
           href="/"
