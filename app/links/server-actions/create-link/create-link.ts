168a5fc071f6a28ac53a4ea006c8c8d0e55f02b8
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-07-06 11:07:33 +0200

    feat: update createLink server action

--- app/links/server-actions/create-link/create-link.ts
+++ app/links/server-actions/create-link/create-link.ts
@@ -2,7 +2,7 @@
 import type { CreateLinkArgs } from './create-link.types';
 import { revalidatePath } from 'next/cache';
 import { isValidUrl } from '@/utils';
-import { nanoid, getMetaTags } from '@/lib-server';
+import { getRandomLinkSlug, getMetaTags } from '@/lib-server';
 import prisma from '@/lib-server/prisma';
 
 export async function createLink(args: CreateLinkArgs) {
@@ -18,30 +18,19 @@ export async function createLink(args: CreateLinkArgs) {
     };
   }
 
-  async function getRandomKey() {
-    const randomKey = nanoid();
-    const linkWithRadomKey = await prisma.link.findUnique({ where: { key: randomKey } });
-
-    if (linkWithRadomKey) {
-      return getRandomKey();
-    }
-
-    return randomKey;
-  }
-
   const metaTags = await getMetaTags(url);
 
   if (!metaTags) {
     throw Error(`[server-actions createLink]: Failed to fetch meta tags from ${url}`);
   }
 
-  const key = await getRandomKey();
+  const randomLinkSlug = await getRandomLinkSlug();
 
   await prisma.link.create({
     data: {
       userId,
-      key,
       url,
+      slug: randomLinkSlug,
       password: password.length ? password : undefined,
       ...metaTags,
     },
