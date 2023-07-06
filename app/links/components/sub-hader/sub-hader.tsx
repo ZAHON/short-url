0bd9148800d7a3bd3b7c2ac8a2e736c5e0e055c6
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-07-06 18:05:17 +0200

    feat: change heading element level in SubHeader component

--- app/links/components/sub-hader/sub-hader.tsx
+++ app/links/components/sub-hader/sub-hader.tsx
@@ -4,7 +4,7 @@ export function SubHeader() {
   return (
     <div className="border-b border-b-accent-2 bg-white">
       <div className="flex h-24 items-center justify-between px-4 lg:mx-auto lg:max-w-7xl">
-        <h1 className="text-2xl font-medium">My links</h1>
+        <h2 className="text-2xl">My links</h2>
         <CreateLinkButton />
       </div>
     </div>
