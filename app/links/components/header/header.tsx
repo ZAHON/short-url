a1eb6fd763ffdd99db936995ba2870e8dfb8b7f1
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-07-06 18:05:52 +0200

    feat: add logo into Header component on links page

--- app/links/components/header/header.tsx
+++ app/links/components/header/header.tsx
@@ -1,10 +1,18 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import { UserMenu } from '..';
 
 export function Header() {
   return (
     <div className="sticky top-0 z-40 border-b border-b-accent-2 bg-white">
       <header className="flex h-14 items-center justify-between px-4 lg:mx-auto lg:max-w-7xl">
-        <div></div>
+        <Link
+          href="/"
+          className="flex select-none items-center gap-x-2 rounded-md text-xl font-medium focus:outline-none focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-success"
+        >
+          <Image height={28} width={28} src="/images/logo.svg" alt="Short URL logo" />
+          Short URL
+        </Link>
         <UserMenu />
       </header>
     </div>
