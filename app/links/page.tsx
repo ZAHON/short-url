b34ef4c841337e9371c9533575e4649a1d9ec88e
Konrad Trusiuk <konrad.zahon@gmail.com>
2023-07-06 11:10:02 +0200

    feat: show link cards on links page

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -1,3 +1,26 @@
+import { redirect } from 'next/navigation';
+import { getServerSession } from '@/lib-server';
+import { getLinks } from './server-actions';
+import { LinkCardsWrapper, LinkCard } from './components';
+
+export const revalidate = 0;
+export const dynamic = 'force-dynamic';
+
 export default async function LinksPage() {
-  return <h1>Links page</h1>;
+  const session = await getServerSession();
+  const userId = session?.user?.id;
+
+  if (!session || !userId) {
+    redirect('/login');
+  }
+
+  const links = await getLinks({ userId });
+
+  return (
+    <LinkCardsWrapper>
+      {links.map((link) => (
+        <LinkCard key={link.id} {...link} />
+      ))}
+    </LinkCardsWrapper>
+  );
 }
